4216365d4eaa520382381ccee1e9f139836c79d1
nomadqwerty <davexmike72@gmail.com>
2022-07-18 10:18:16 +0100

    ClassComps And State

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -1,15 +1,23 @@
-import React from 'react';
+import {React,Component} from 'react';
 import ReactDOM from 'react-dom/client';
 
 
 const el = document.getElementById('root')
 const root = ReactDOM.createRoot(el);
 
-const App = ()=>{
-  window.navigator.geolocation.getCurrentPosition((position)=>{
-    console.log(position)
-  },(err)=>{console.log(err)})
+// xtend react Comp class
+// recat.comp gives use nesassary methods our class needs to work
+class App extends Component{
+  // it has a render method
+  render(){
+    window.navigator.geolocation.getCurrentPosition((position)=>{
+      console.log(position)
+    },(err)=>{console.log(err)})
+
+    return(<div>Lat</div>)
+  }
 }
+
 root.render(<App/>)
 
 
