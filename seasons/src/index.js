c4362a8364d5a2a20d4d4d54313cccd6963d8fa0
nomadqwerty <davexmike72@gmail.com>
2022-07-19 10:25:11 +0100

    stateInit

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -8,13 +8,19 @@ const root = ReactDOM.createRoot(el);
 // xtend react Comp class
 // recat.comp gives use nesassary methods our class needs to work
 class App extends Component{
+  constructor(props){
+    super(props)
+    this.state={lat:null,lng:null}
+
+    window.navigator.geolocation.getCurrentPosition((position)=>{
+      let latitude = position.coords.latitude
+      let longitude = position.coords.longitude
+      this.setState({lat:latitude,lng:longitude})
+    },(err)=>{console.log(err)})
+  }
   // it has a render method
   render(){
-    window.navigator.geolocation.getCurrentPosition((position)=>{
-      console.log(position)
-    },(err)=>{console.log(err)})
-
-    return(<div>Lat</div>)
+    return(<div>LAT:{this.state.lat}, LNG:{this.state.lng}</div>)
   }
 }
 
