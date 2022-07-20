0e2629c009edeae5e3b5c75e5d7a2f7e10f6ac4c
nomadqwerty <davexmike72@gmail.com>
2022-07-20 16:02:59 +0100

    compLifecycle

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -10,16 +10,30 @@ const root = ReactDOM.createRoot(el);
 class App extends Component{
   constructor(props){
     super(props)
-    this.state={lat:null,lng:null}
+    this.state={lat:null,lng:null,errorMsg:''}
 
-    window.navigator.geolocation.getCurrentPosition((position)=>{
-      let latitude = position.coords.latitude
-      let longitude = position.coords.longitude
-      this.setState({lat:latitude,lng:longitude})
-    },(err)=>{console.log(err)})
+    
+      window.navigator.geolocation.getCurrentPosition((position)=>{
+        let latitude = position.coords.latitude
+        let longitude = position.coords.longitude
+        this.setState({lat:latitude,lng:longitude})
+      },(err)=>{
+        console.log(err)
+        this.setState({errorMsg:err.message})
+      })
+    
+      
+    
   }
+  
   // it has a render method
   render(){
+    if(!this.state.lat && !this.state.lng && !this.state.errorMsg){
+      return(<div>loading...</div>)
+    }
+    if(this.state.errorMsg){
+      return(<div>please try again {this.state.errorMsg}</div>)
+    }
     return(<div>LAT:{this.state.lat}, LNG:{this.state.lng}</div>)
   }
 }
