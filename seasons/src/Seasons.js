e422fed36bbc72a2a9165c16bf1237b925db0c18
nomadqwerty <davexmike72@gmail.com>
2022-07-22 15:49:49 +0100

    seasons

--- seasons/src/Seasons.js
+++ seasons/src/Seasons.js
@@ -1,9 +1,36 @@
 import React from 'react';
 
+let seasonObj = {
+    summer:{
+        text:' summer in nigeria is wet and hot',
+        iconName:'sun'
+    },
+    winter:{
+        text:' summer in nigeria is wet and hot',
+        iconName:'snowflake'
+    }
+}
 
-
+const getSeason = (lat)=>{
+    let month = new Date().getMonth()
+    if(month>2&&month<9){
+        return lat > 0? 'summer' : 'hamatan'
+    }else{
+        return lat > 0? 'summer' : 'hamatan'
+    }
+}
 const Seasons = (props)=>{
-    return (<div>Seasons, lat is: {props.lat} & lng is: {props.lng}</div>)
+    let season = getSeason(props.lat)
+    let {text, iconName} = seasonObj[season]
+    return (
+        <div> 
+            <i className={`${iconName} icon`}></i>
+            <h1>
+                {text}
+            </h1>
+            <i className={`${iconName} icon`}></i>
+        </div>
+    )
 }
 
 
