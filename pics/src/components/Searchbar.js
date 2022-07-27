d290824ee1e21b2886065670ab16692170baea79
nomadqwerty <davexmike72@gmail.com>
2022-07-27 17:24:49 +0100

    26.7

--- pics/src/components/Searchbar.js
+++ pics/src/components/Searchbar.js
@@ -1,13 +1,19 @@
 import React from 'react';
 
 class SearchBar extends React.Component {
+    state = {input:''}
+    onformSubmit(e){
+        e.preventDefault();
+        console.log('submitted')
+    }
     render(){
+        console.log(this.state.input)
         return(
-            <div class="ui left icon input loading">
-                <form className='ui form'>
+            <div className="ui left icon input loading">
+                <form className='ui form' onSubmit={this.onformSubmit}>
                     <div className='field'>
                         <label>image search</label>
-                        <input type="text" placeholder="Search..."/>
+                        <input type="text" placeholder="Search..." value={this.state.input} onChange={(e)=>this.setState({input:e.target.value})}/>
                     </div>
                 </form>
             </div>
