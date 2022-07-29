9e3ea461c45c5e8e4c2749058fb5b57f83903861
nomadqwerty <davexmike72@gmail.com>
2022-07-29 15:26:29 +0100

    29.7

--- pics/src/components/app.js
+++ pics/src/components/app.js
@@ -1,9 +1,18 @@
 import React from 'react'
+import axios from 'axios'
 import SearchBar from './Searchbar'
 
 class App extends React.Component{
-    apiCall = (input)=>{
-        console.log(input)
+    apiCall = async (input)=>{
+        let data = await axios.get(' https://api.unsplash.com/search/photos',{
+            params:{
+                query:input
+            },
+            headers:{
+                Authorization:'Client-ID oducr95UOqhhHjU10oU5uaLofUg0TOhC_yp3rxz1KSI'
+            }
+        })
+        console.log(data)
     }
     render(){
         return(
