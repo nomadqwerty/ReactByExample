bbd04cc2b9391c3223903a0f704135256720a34f
nomadqwerty <davexmike72@gmail.com>
2022-08-16 15:21:35 +0100

    16.8

--- tyson_hooks/src/components/accordian.js
+++ tyson_hooks/src/components/accordian.js
@@ -1,10 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Accordion = (props) => {
+  const [selectedIndex, setSelectedIndex] = useState(null);
+  let onTitleclick = (index) => {
+    setSelectedIndex(index);
+    console.log(useState);
+  };
+
   let list = props.items.map((item, i) => {
     return (
       <React.Fragment key={item.title}>
-        <div className="title active">
+        <div
+          className="title active"
+          onClick={() => {
+            onTitleclick(i);
+          }}
+        >
           <i className="dropdown icon"></i>
           {item.title}
         </div>
@@ -14,7 +25,12 @@ const Accordion = (props) => {
       </React.Fragment>
     );
   });
-  return <div className="ui styled accordion">{list}</div>;
+  return (
+    <div className="ui styled accordion">
+      {list}
+      <h1>{selectedIndex}</h1>
+    </div>
+  );
 };
 
 export default Accordion;
